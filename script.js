e7b01ca06523e47bda38a1d9547a28897a10826e
Syazwan Fitri <syazfitry69@gmail.com>
2022-01-04 16:39:58 +0800

    Added eventListener to hamburger

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const hamburger = document.querySelector('.hamburger-link');
 const mobileNav = document.querySelector('.nav-links');
+const hamburgerIcon = document.querySelector('.hamburger-icon');
 
-hamburger.addEventListener('click', function(e) {
-   mobileNav.classList.remove('display-none');
-   e.preventDefault();
-});
+const btnNav = (() => {
+    let count = 0;
+    function slide(e) {
+        hamburger.classList.toggle('isActive');
+        if(count === 0) {
+            mobileNav.classList.remove('display-none');
+            hamburgerIcon.src = './images/icon-close.svg';
+            count += 1;
+            console.log(count);
+            e.preventDefault();
+            return;
+        }
+        if(count !== 0) {
+            mobileNav.classList.add('display-none');
+            hamburgerIcon.src = './images/icon-hamburger.svg';
+            count -= 1;
+            console.log(count);
+            e.preventDefault();
+            return;
+        }       
+    }
+    return slide;
+})();
+
+hamburger.addEventListener('click', btnNav);
